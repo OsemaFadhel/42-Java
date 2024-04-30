b1c31d768d8c96ad354ad544d65710818f8be431
Osema Fadhel <ofadhel@e3r6p1.42roma.it>
2024-04-30 05:50:34 +0200

    java

--- Java01/ex00/Transaction.java
+++ Java01/ex00/Transaction.java
@@ -6,7 +6,7 @@
 /*   By: ofadhel <ofadhel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 17:57:31 by ofadhel           #+#    #+#             */
-/*   Updated: 2024/04/24 17:57:31 by ofadhel          ###   ########.fr       */
+/*   Updated: 2024/04/30 05:49:45 by ofadhel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,17 @@ public class Transaction
 
 	public Transaction(User recipient, User sender, String transferCategory, double transferAmount)
 	{
+		if (transferAmount <= 0)
+			throw new IllegalArgumentException("Transfer amount must be greater than 0");
+		if (sender.getBalance() < transferAmount)
+			throw new IllegalArgumentException("Sender does not have enough balance to make this transfer");
 		this.identifier = UUID.randomUUID();
 		this.recipient = recipient;
 		this.sender = sender;
 		this.transferCategory = transferCategory;
 		this.transferAmount = transferAmount;
+		sender.setBalance(sender.getBalance() - transferAmount);
+		recipient.setBalance(recipient.getBalance() + transferAmount);
 	}
 	public UUID getIdentifier()
 		return identifier;
--- Java01/ex00/User.java
+++ Java01/ex00/User.java
@@ -6,18 +6,20 @@
 /*   By: ofadhel <ofadhel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 17:55:09 by ofadhel           #+#    #+#             */
-/*   Updated: 2024/04/24 17:55:09 by ofadhel          ###   ########.fr       */
+/*   Updated: 2024/04/30 05:48:04 by ofadhel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 public class User
 {
-	private int id;
+	private int identifier;
 	private String name;
 	private double balance;
 
 	public User(int identifier, String name, double balance)
 	{
+		if (balance < 0)
+			throw new IllegalArgumentException("Balance cannot be negative");
 		this.identifier = identifier;
 		this.name = name;
 		this.balance = balance;
