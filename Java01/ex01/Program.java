fde98a14f51f4e72f69440566cd2162868d470fe
Osema Fadhel <ofadhel@e3r2p8.42roma.it>
2024-05-07 09:52:57 +0200

    java

--- Java01/ex01/Program.java
+++ Java01/ex01/Program.java
@@ -6,18 +6,18 @@
 /*   By: ofadhel <ofadhel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 16:10:31 by ofadhel           #+#    #+#             */
-/*   Updated: 2024/04/30 17:23:38 by ofadhel          ###   ########.fr       */
+/*   Updated: 2024/05/07 09:52:33 by ofadhel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 public class Program {
-    public static void main(String[] args) {
-        User user1 = new User("Alice", 1000.0);
-        User user2 = new User("Bob", 2000.0);
+	public static void main(String[] args) {
+		User user1 = new User("Alice", 1000.0);
+		User user2 = new User("Bob", 2000.0);
 		User user3 = new User("Charlie", 3000.0);
 
-        System.out.println("User 1 ID: " + user1.getIdentifier());
-        System.out.println("User 2 ID: " + user2.getIdentifier());
+		System.out.println("User 1 ID: " + user1.getIdentifier());
+		System.out.println("User 2 ID: " + user2.getIdentifier());
 		System.out.println("User 3 ID: " + user3.getIdentifier());
-    }
+	}
 }
--- Java01/ex03/TransactionsLinkedList.java
+++ Java01/ex03/TransactionsLinkedList.java
@@ -6,72 +6,70 @@
 /*   By: ofadhel <ofadhel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 21:42:31 by ofadhel           #+#    #+#             */
-/*   Updated: 2024/04/30 22:13:35 by ofadhel          ###   ########.fr       */
+/*   Updated: 2024/05/07 09:51:51 by ofadhel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 import java.util.UUID;
 
 public class TransactionsLinkedList implements TransactionsList {
-    private Transaction head;
+	private Transaction head;
+	@Override
+	public void addTransaction(Transaction transaction) {
+		if (head == null) {
+			head = transaction;
+		} else {
+			Transaction current = head;
+			while (current.getNextTransaction() != null) {
+				current = current.getNextTransaction();
+			}
+			current.setNextTransaction(transaction);
+		}
+	}
 
-    @Override
-    public void addTransaction(Transaction transaction) {
-        if (head == null) {
-            head = transaction;
-        } else {
-            Transaction current = head;
-            while (current.getNextTransaction() != null) {
-                current = current.getNextTransaction();
-            }
-            current.setNextTransaction(transaction);
-        }
-    }
+	@Override
+	public void removeTransaction(UUID id) throws TransactionNotFoundException {
+		if (head == null) {
+			throw new TransactionNotFoundException("Transaction with ID " + id + " not found");
+		}
 
-    @Override
-    public void removeTransaction(UUID id) throws TransactionNotFoundException {
-        if (head == null) {
-            throw new TransactionNotFoundException("Transaction with ID " + id + " not found");
-        }
+		if (head.getIdentifier().equals(id)) {
+			head = head.getNextTransaction();
+			return;
+		}
 
-        if (head.getIdentifier().equals(id)) {
-            head = head.getNextTransaction();
-            return;
-        }
+		Transaction current = head;
+		while (current.getNextTransaction() != null) {
+			if (current.getNextTransaction().getIdentifier().equals(id)) {
+				current.setNextTransaction(current.getNextTransaction().getNextTransaction());
+				return;
+			}
+			current = current.getNextTransaction();
+		}
 
-        Transaction current = head;
-        while (current.getNextTransaction() != null) {
-            if (current.getNextTransaction().getIdentifier().equals(id)) {
-                current.setNextTransaction(current.getNextTransaction().getNextTransaction());
-                return;
-            }
-            current = current.getNextTransaction();
-        }
+		throw new TransactionNotFoundException("Transaction with ID " + id + " not found");
+	}
 
-        throw new TransactionNotFoundException("Transaction with ID " + id + " not found");
-    }
+	@Override
+	public Transaction[] toArray() {
+		if (head == null) {
+			return new Transaction[0];
+		}
 
-    @Override
-    public Transaction[] toArray() {
-        if (head == null) {
-            return new Transaction[0];
-        }
+		int size = 0;
+		Transaction current = head;
+		while (current != null) {
+			size++;
+			current = current.getNextTransaction();
+		}
 
-        int size = 0;
-        Transaction current = head;
-        while (current != null) {
-            size++;
-            current = current.getNextTransaction();
-        }
-
-        Transaction[] transactionsArray = new Transaction[size];
-        current = head;
-        int index = 0;
-        while (current != null) {
-            transactionsArray[index++] = current;
-            current = current.getNextTransaction();
-        }
-
-        return transactionsArray;
-    }
+		Transaction[] transactionsArray = new Transaction[size];
+		current = head;
+		int index = 0;
+		while (current != null) {
+			transactionsArray[index++] = current;
+			current = current.getNextTransaction();
+		}
+		return transactionsArray;
+	}
 }
